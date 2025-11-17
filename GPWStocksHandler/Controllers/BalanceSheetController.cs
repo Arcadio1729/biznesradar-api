8bb8445deb2efd02b88923d791e96caa29ee346a
Arkadiusz Dutka <adutka@ad.pgd>
2025-11-17 19:45:13 +0100

    first commit

---
+++ GPWStocksHandler/Controllers/BalanceSheetController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using System.Text;
+using System.Text.Json;
+
+namespace GPWStocksHandler.Controllers
+{
+    [ApiController]
+    [Route("api/balance-sheet")]
+    public class BalanceSheetController : ControllerBase
+    {
+        [HttpGet]
+        [Route("current-assets")]
+        public async Task<IActionResult> GetAssets(string ticker="CPS")
+        {
+            using var client = new HttpClient();
+
+            string url = $"https://www.biznesradar.pl/raporty-finansowe-bilans/{ticker}"; 
+
+            string html = await client.GetStringAsync(url);
+
+            
+            return Ok(GetTable(html));
+        }
+
+
+        private string GetTable(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            HtmlNode table = doc.DocumentNode.SelectSingleNode("//table[contains(concat(' ', normalize-space(@class), ' '), ' report-table ')]");
+            var tables = doc.DocumentNode.SelectNodes("//table");
+
+            StringBuilder data=new StringBuilder();
+            if (table != null)
+            {
+                var years = table.SelectNodes(".//th[contains(@class, 'thq h')]");
+                var row = table.SelectSingleNode(".//tr[@data-field='BalanceCurrentAssets']");
+
+                var span = row.SelectNodes(".//span[contains(@class, 'value')]").Select(s=>s.InnerText.Trim());
+               
+                var newYears = new List<string>();
+                foreach(var y in years)
+                {
+                    string directText = string.Concat(
+                            y.ChildNodes
+                                .Where(n => n.NodeType == HtmlNodeType.Text)
+                                .Select(n => n.InnerText)
+                        ).Trim();
+
+                    newYears.Add(directText);
+                }
+
+
+                var items = span.Zip(newYears, (price, year) => new { price,year});
+
+                return JsonSerializer.Serialize(items);   
+            }
+
+            return "Table null";
+        }
+    }
+}
